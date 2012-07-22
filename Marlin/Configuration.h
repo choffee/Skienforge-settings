259d43bf4db2cbca724761b61396cbae12ffb6a9
John Cooper <john@choffee.co.uk>
2012-07-22 12:20:53 +0100

    Some more tuning of the config for Marlin firmware

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -214,7 +214,7 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 
 // default settings 
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.4094,80.4094,2927.1862,760*1.1*1.1306}  // default steps per unit for ultimaker 
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.4094,80.4094,2927.1862,771.58}  // default steps per unit for ultimaker 
 // #define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,200*8/3,760*1.1}  // default steps per unit for ultimaker 
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 45}    // (mm/sec)    
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
